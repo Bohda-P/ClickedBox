e398817028d438084fcde3c3dbac30cfff5da170
Bohdan <bogdanpaseka2000@gmail.com>
2020-09-04 15:12:51 +0300

    initial

---
+++ index.js
@@ -0,0 +1,102 @@
+const $start = document.querySelector('#start')
+const $game = document.querySelector('#game')
+const $time = document.querySelector('#time')
+const $result = document.querySelector('#result')
+const $timeHeader = document.querySelector('#time-header')
+const $resultHeader = document.querySelector('#result-header')
+const $gameTime = document.querySelector('#game-time')
+
+function show(el) {
+  el.classList.remove('hide')
+}
+
+function hide(el) {
+  el.classList.add('hide')
+}
+
+const colors = ['red', 'blue', 'green', 'yellow', 'pink']
+let score = 0;
+let isGameStarted = false;
+
+$start.addEventListener('click', () => {
+  score = 0;
+  setGameTime()
+  $gameTime.setAttribute('disabled', 'true')
+  isGameStarted = true;
+  $game.style.backgroundColor = '#fff'
+  $start.classList.add('hide')
+
+  let interval = setInterval(() => {
+    let time = parseFloat($time.textContent)
+    if (time <= 0) {
+      clearInterval(interval)
+      endGame()
+    } else {
+      $time.textContent = (time - 0.1).toFixed(1)
+    }
+
+  }, 100)
+
+  renderBox()
+})
+
+$gameTime.addEventListener('input', setGameTime)
+
+function setGameScore() {
+  $result.textContent = score.toString()
+}
+
+function setGameTime() {
+  let time = +$gameTime.value;
+  $time.textContent = time.toFixed(1)
+  show($timeHeader)
+  hide($resultHeader)
+}
+
+function endGame() {
+  isGameStarted = false;
+  setGameScore()
+  show($start)
+  $game.innerHTML = ''
+  $game.style.backgroundColor = '#ccc'
+  hide($timeHeader)
+  show($resultHeader)
+  $gameTime.removeAttribute('disabled')
+}
+
+$game.addEventListener('click', (event) => {
+  if (!isGameStarted) {
+    return
+  }
+
+  if (event.target.dataset) {
+
+    score++;
+    renderBox()
+  }
+})
+
+function renderBox() {
+  $game.innerHTML = ''
+  const box = document.createElement('div');
+  let boxSize = getRandom(30, 100)
+  let gameSize = $game.getBoundingClientRect()
+  let randomColor = getRandom(0, colors.length)
+
+  const maxTop = gameSize.height - boxSize
+  const maxLeft = gameSize.width - boxSize
+
+  box.style.height = box.style.width = boxSize + 'px';
+  box.style.position = 'absolute';
+  box.style.backgroundColor = colors[randomColor];
+  box.style.top = getRandom(0, maxTop) + 'px'
+  box.style.left = getRandom(0, maxLeft) + 'px'
+  box.style.cursor = 'pointer'
+  box.setAttribute('data-box', 'true')
+
+  $game.insertAdjacentElement("afterbegin", box);
+}
+
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min)
+}
